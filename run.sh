be30b33d40c87e56f5407659efa7ff5f7d01f68b
Laurie Ho <gftw82@ncc1.clients.dur.ac.uk>
2020-12-21 01:42:55 +0000

    run script tweaks

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=short
 #SBATCH -t 01-00:00:00
 #SBATCH --job-name=pix2pixLaurie
-#SBATCH --mem=32G
+#SBATCH --mem=22G
 #SBATCH --mail-user laurence.ho@durham.ac.uk
 #SBATCH --mail-type=ALL
 
@@ -16,4 +16,4 @@ module load cuda/10.1-cudnn7.6
 
 python train.py --dataroot ./datasets/sketchy --name sketchy_pix2pix --model pix2pix --direction BtoA --display_id=0
 
-python train.py --dataroot ./datasets/sketchy --name sketchy_pix2pix --model pix2pix --direction BtoA --display_id=0
+python test.py --dataroot ./datasets/sketchy --name sketchy_pix2pix --model pix2pix --direction BtoA
